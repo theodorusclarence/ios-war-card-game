103486b5461907580cff2056a33422d54ecdf5ba
Theodorus Clarence <theodorusclarence@gmail.com>
2022-03-13 20:48:15 +0700

    feat: add game ui

---
+++ war-card-game/ContentView.swift
@@ -0,0 +1,59 @@
+//
+//  ContentView.swift
+//  war-card-game
+//
+//  Created by Clarence on 13/03/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ZStack() {
+            Image("background")
+                .ignoresSafeArea()
+            
+            VStack() {
+                Spacer()
+                Image("logo")
+                Spacer()
+                HStack() {
+                    Spacer()
+                    Image("card3")
+                    Spacer()
+                    Image("card4")
+                    Spacer()
+                }
+                Spacer()
+                Image("dealbutton")
+                Spacer()
+                HStack() {
+                    Spacer()
+                    VStack() {
+                        Text("Player")
+                            .font(.headline)
+                        Text("0")
+                            .font(.largeTitle)
+                            .padding(.top, 10)
+                    }.foregroundColor(Color.white)
+                    Spacer()
+                    VStack() {
+                        Text("CPU")
+                            .font(.headline)
+                        Text("0")
+                            .font(.largeTitle)
+                            .padding(.top, 10)
+                    }.foregroundColor(Color.white)
+                    Spacer()
+                }
+                Spacer()
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ war-card-game/war_card_gameApp.swift
@@ -0,0 +1,17 @@
+//
+//  war_card_gameApp.swift
+//  war-card-game
+//
+//  Created by Clarence on 13/03/22.
+//
+
+import SwiftUI
+
+@main
+struct war_card_gameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
